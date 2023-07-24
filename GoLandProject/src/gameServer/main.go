48d600c60771c6e3cbc07b6422b9e1b98dd88fa9
limike_4070 <limike99@foxmail.com>
2023-07-25 00:15:15 +0900

    1.优化一下引导

--- GoLandProject/src/gameServer/main.go
+++ GoLandProject/src/gameServer/main.go
@@ -19,54 +19,6 @@ func FormatAsDate(t time.Time) string {
 	return fmt.Sprintf("%d-%02d-%02d", year, month, day)
 }
 func main() {
-	//分组控制
-	//r := geeServer.New()
-	//r.GET("/index", func(c *geeServer.Context) {
-	//	c.HTML(http.StatusOK, "<h1>Index Page</h1>")
-	//})
-	//v1 := r.Group("/v1")
-	//{
-	//	v1.GET("/", func(c *geeServer.Context) {
-	//		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
-	//	})
-	//
-	//	v1.GET("/hello", func(c *geeServer.Context) {
-	//		// expect /hello?name=geektutu
-	//		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
-	//	})
-	//}
-	//v2 := r.Group("/v2")
-	//{
-	//	v2.GET("/hello/:name", func(c *geeServer.Context) {
-	//		// expect /hello/geektutu
-	//		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
-	//	})
-	//	v2.POST("/login", func(c *geeServer.Context) {
-	//		c.JSON(http.StatusOK, geeServer.H{
-	//			"username": c.PostForm("username"),
-	//			"password": c.PostForm("password"),
-	//		})
-	//	})
-	//
-	//}
-
-	//中间件使用
-	//r := geeServer.New()
-	////全局中间件
-	//r.Use(geeServer.Logger())
-	//r.GET("/", func(c *geeServer.Context) {
-	//	c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
-	//})
-	//v2 := r.Group("/v2")
-	//v2.Use(onlyForV2()) // v2 group middleware
-	//{
-	//	v2.GET("/hello/:name", func(c *geeServer.Context) {
-	//		// expect /hello/geektutu
-	//		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
-	//	})
-	//}
-	//r.POST("/login",func(c *geeServer.Context))
-
 	//html使用1
 	r := geeServer.New()
 	r.Use(geeServer.Logger())
@@ -75,23 +27,6 @@ func main() {
 	})
 	r.LoadHTMLGlob("templates/*")
 	r.Static("/assets", "./static")
-	stu1 := &student{Name: "Geektutu", Age: 20}
-	stu2 := &student{Name: "Jack", Age: 22}
-	r.GET("/", func(c *geeServer.Context) {
-		c.HTML(http.StatusOK, "css.html", nil)
-	})
-	r.GET("/stu", func(c *geeServer.Context) {
-		c.HTML(http.StatusOK, "arr.html", geeServer.H{
-			"title":  "gee",
-			"stuArr": [2]*student{stu1, stu2},
-		})
-	})
-	r.GET("/date", func(c *geeServer.Context) {
-		c.HTML(http.StatusOK, "custom_func.tmpl", geeServer.H{
-			"title": "gee",
-			"now":   time.Date(2019, 8, 17, 0, 0, 0, 0, time.UTC),
-		})
-	})
 	r.GET("/ball", func(c *geeServer.Context) {
 		c.HTML(http.StatusOK, "index.html", geeServer.H{
 			"title": "ballGame",
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # GoLandProject
-study Go！！！
+
+## 简单做了一个基于gin的后端框架，并部署一些自己写过的Unity游戏，补充中...
+
+[1.Unity游戏后端](/GoLandProject/src/gameServer)
+
+[2.Unity游戏-BallGame](/GoLandProject/src/gameServer/static/Ball_web04)
